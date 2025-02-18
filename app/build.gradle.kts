82f76d5440a9067a14bd9b6c1a97cd6eb1db4230
bpetr77 <malom.tg@gmail.com>
2025-02-18 14:54:00 +0100

    Térkép implementálása

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -47,6 +48,17 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    secrets {
+        // To add your Maps API key to this project:
+        // 1. If the secrets.properties file does not exist, create it in the same folder as the local.properties file.
+        // 2. Add this line, where YOUR_API_KEY is your API key:
+        //        MAPS_API_KEY=YOUR_API_KEY
+        propertiesFileName = "secrets.properties"
+
+        // A properties file containing default secret values. This file can be
+        // checked in version control.
+        defaultPropertiesFileName = "local.defaults.properties"
+    }
 }
 
 dependencies {
@@ -59,6 +71,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.maps.android:maps-compose:2.5.3")
+    //implementation("com.google.accompanist:accompanist-permissions:0.32.0") // Engedélykéréshez
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/hu/bme/aut/android/sporttracker/ui/screens/map/Map_window.kt
+++ app/src/main/java/hu/bme/aut/android/sporttracker/ui/screens/map/Map_window.kt
@@ -1,4 +1,4 @@
-package hu.bme.aut.android.sporttracker
+package hu.bme.aut.android.sporttracker.ui.screens.map
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,20 +11,12 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
-import hu.bme.aut.android.sporttracker.ui.screens.map.MapScreen
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MapScreen2()
-        }
-    }
-}
 
 @Composable
-fun MapScreen2() {
+fun MapScreen() {
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(LatLng(47.4979, 19.0402), 10f)
     }
