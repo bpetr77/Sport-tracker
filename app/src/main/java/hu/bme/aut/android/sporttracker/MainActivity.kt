d5270e14b51c4702d1874ab1441fb6b706c18b26
bpetr77 <malom.tg@gmail.com>
2025-02-18 09:06:12 +0100

    user name fix

--- app/src/main/java/hu/bme/aut/android/sporttracker/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/sporttracker/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SportTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "bpetr77",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
