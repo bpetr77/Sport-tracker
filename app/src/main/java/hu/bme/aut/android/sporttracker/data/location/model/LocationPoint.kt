ba5b56db9c7c3e3bf993f64be839a4644bb12e19
Bakó Péter <malom.tg@gmail.com>
2025-03-02 21:45:06 +0100

    ...

--- app/src/main/java/hu/bme/aut/android/sporttracker/data/location/model/LocationPoint.kt
+++ app/src/main/java/hu/bme/aut/android/sporttracker/data/location/model/LocationPoint.kt
@@ -1,2 +1,7 @@
 package hu.bme.aut.android.sporttracker.data.location.model
 
+data class LocationPoint(
+    val latitude: Double,
+    val longitude: Double,
+    val timestamp: Long
+)
--- app/src/main/java/hu/bme/aut/android/sporttracker/data/location/repository/LocationRepository.kt
+++ app/src/main/java/hu/bme/aut/android/sporttracker/data/location/repository/LocationRepository.kt
@@ -1,2 +1,17 @@
 package hu.bme.aut.android.sporttracker.data.location.repository
 
+import hu.bme.aut.android.sporttracker.data.location.model.LocationPoint
+
+object LocationRepository {
+    private val _points = mutableListOf<LocationPoint>()
+
+    fun addPoint(point: LocationPoint) {
+        _points.add(point)
+    }
+
+    fun getAllPoints(): List<LocationPoint> = _points.toList()
+
+    fun clear() {
+        _points.clear()
+    }
+}
