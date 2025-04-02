49383f78ee3c1ad157d6ae03e47273033442922a
bpetr77 <malom.tg@gmail.com>
2025-04-02 22:58:25 +0200

    start of the tour overview but its not appearing..

--- app/src/main/java/hu/bme/aut/android/sporttracker/ui/navigation/NavGraph.kt
+++ app/src/main/java/hu/bme/aut/android/sporttracker/ui/navigation/NavGraph.kt
@@ -8,9 +8,11 @@ import androidx.navigation.compose.rememberNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import hu.bme.aut.android.sporttracker.MainActivity
 import hu.bme.aut.android.sporttracker.data.location.repository.LocationRepository
+import hu.bme.aut.android.sporttracker.data.tour.repository.TourRepository
 import hu.bme.aut.android.sporttracker.domain.usecase.TourUseCase
 import hu.bme.aut.android.sporttracker.ui.screens.main.MainScreen
 import hu.bme.aut.android.sporttracker.ui.screens.menu.MenuScreen
+import hu.bme.aut.android.sporttracker.ui.screens.menu.TourMenuScreen
 import hu.bme.aut.android.sporttracker.ui.viewModels.LocationViewmodel
 import hu.bme.aut.android.sporttracker.ui.viewModels.TourSettingsViewModel
 import hu.bme.aut.android.sporttracker.ui.viewModels.TourStartedSettingsViewModel
@@ -24,7 +26,8 @@ fun NavGraph(
     tourSettingsViewModel: TourSettingsViewModel,
     tourStartedSettingsViewModel: TourStartedSettingsViewModel,
     locationViewmodel: LocationViewmodel,
-    tourUseCase: TourUseCase
+    tourUseCase: TourUseCase,
+    tourRepository: TourRepository
 ) {
     NavHost(
         navController = navController,
@@ -39,11 +42,16 @@ fun NavGraph(
                 tourStartedSettingsViewModel = tourStartedSettingsViewModel,
                 locationViewmodel = locationViewmodel,
                 onMenuclick = { navController.navigate(Screen.Menu.route) },
-                tourUseCase = tourUseCase
+                tourUseCase = tourUseCase,
+                onToursClick = { navController.navigate(Screen.Tours.route) }
             )
         }
         composable(Screen.Menu.route){
             MenuScreen()
+        }
+
+        composable(Screen.Tours.route) {
+            TourMenuScreen(tourRepository = tourRepository)
         }
     }
 }
--- app/src/main/java/hu/bme/aut/android/sporttracker/ui/navigation/Screen.kt
+++ app/src/main/java/hu/bme/aut/android/sporttracker/ui/navigation/Screen.kt
@@ -3,5 +3,5 @@ package hu.bme.aut.android.sporttracker.ui.navigation
 sealed class Screen(val route: String) {
     object Main : Screen("main")
     object Menu: Screen("menu")
-    object Screen2: Screen("screen2")
+    object Tours: Screen("tours")
 }
