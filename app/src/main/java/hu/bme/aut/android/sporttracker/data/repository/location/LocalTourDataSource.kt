1b6ac9293830421c3cded7016d74ccb3cf76684c
Bakó Péter <malom.tg@gmail.com>
2025-11-05 15:19:32 +0100

    lot of refactor but some to continue, to sum it up: makeing interfaces for repos

---
+++ app/src/main/java/hu/bme/aut/android/sporttracker/data/repository/location/LocalTourDataSource.kt
@@ -0,0 +1,20 @@
+package hu.bme.aut.android.sporttracker.data.repository.location
+
+import hu.bme.aut.android.sporttracker.data.local.tour.model.TourEntity
+import hu.bme.aut.android.sporttracker.data.local.tour.database.TourDao
+
+class LocationTourDataSource(private val tourDao: TourDao) {
+    suspend fun addTour(tour: TourEntity) = tourDao.insertTour(tour)
+
+    suspend fun getTourById(id: Long) = tourDao.getTourById(id)
+
+    suspend fun getAllTours() = tourDao.getAllTours()
+
+    suspend fun deleteAllTours() = tourDao.deleteAllTours()
+
+    suspend fun updateTour(tour: TourEntity) = tourDao.updateTour(tour)
+
+    suspend fun deleteTourById(id: Long) = tourDao.deleteTourById(id)
+
+    suspend fun getToursByUser(userId: String) = tourDao.getToursByUser(userId)
+}
